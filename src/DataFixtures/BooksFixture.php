055d7a4f5c4370340c67253a26509d6573aae9f8
Mohamed-Amini2 <mohamedamini740+@gmail.com>
2025-04-07 03:20:45 +0700

    just deleting some useless things

--- src/DataFixtures/BooksFixture.php
+++ src/DataFixtures/BooksFixture.php
@@ -16,25 +16,6 @@ class BooksFixture extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        $Book = new Books();
-        $Book->setTitle('The life');
-        $Book->setAuthor($this->getReference('Author_1' , Author::class));
-        $Book->setISBN('asdsadjaslkjdlaksjdlsajldjasldsa');
-        $Book->setGenre('horror');
-        $Book->setPublicationDate(new DateTimeImmutable('1952-02-05'));
-        $Book->setBookImage('asaasasasasaassasaa');
-        $manager->persist($Book);
-
-        $Book2 = new Books();
-        $Book2->setTitle('The life');
-        $Book2->setAuthor($this->getReference('Author_2' , Author::class));
-        $Book2->setISBN('asdsadjaslkjdlaksjdlsajldjasldsa');
-        $Book2->setGenre('philosophie');
-        $Book2->setPublicationDate(new DateTimeImmutable('1933-02-12'));
-        $Book2->setBookImage('asaasasasasaassasaa');
-        $manager->persist($Book2);
-
-
         $manager->flush();
     }
 }
