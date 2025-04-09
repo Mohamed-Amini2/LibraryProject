eb5e0e3cbdb50edc862c84078bdf085d5136f024
Mohamed-Amini2 <mohamedamini740+@gmail.com>
2025-04-09 11:39:46 +0700

    now we work on the JWT AUTH

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Repository\BooksRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
@@ -70,9 +71,50 @@ final class AuthorController extends AbstractController
             'FirstName' => $author->getFirstName(),
             'LastName' => $author->getLastName(),
             'Books' => $bookTitles,
-        ];
-
-        
+        ];        
         return new JsonResponse($authorData , Response::HTTP_OK);
+    }
+
+    /**
+     * @param is the fucking $data and the fucking $author
+     * 
+     * here we do the POST or ADD author
+     */
+    #[Route('/delete/{id}' , methods:['DELETE'] , name:'app_author_delete')]
+    public function deleteAuthor($id): JsonResponse
+    {
+        $author = $this->AuthorRepo->find($id);
+        if(!$author){
+            return new JsonResponse(['error' => 'The Author Was not Found To Delete'],Response::HTTP_NOT_FOUND);
+        }
+
+        $this->em->remove($author);
+        $this->em->flush();
+
+        return new JsonResponse(['Sucess' => 'The Author Was Succesfuly Delete'], Response::HTTP_OK);
+    }
+
+    #[Route('/edit/{id}' , methods:['PATCH'] , name: 'app_author_edit')]
+    public function editauthor($id , Request $request): JsonResponse
+    {
+        $author = $this->AuthorRepo->find($id);
+        if(!$author){
+            return new JsonResponse(['error' => 'The Author Was not Found To Delete'],Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            $this->serializer->deserialize(
+                $request->getContent(),
+                Author::class,
+                'json',
+                ['object_to_populate' => $author]
+            );
+
+        $this->em->flush();
+        }catch(\JsonException $e) { 
+            return new JsonResponse(['error' => 'Invalid JSON'], 400);
+        }
+
+        return new JsonResponse($author , 200);
     }
 }
